23ca06356b88ebd8da2c08b0b24b8eedc5c27379
RuanDevz <Ruanbatista1509@outlook.com>
2024-06-15 01:13:24 -0300

    feat: headercomponent

---
+++ ecomerce/tailwind.config.js
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      fontFamily:{
+        primary: 'Poppins, sans-serif'
+      },
+      backgroundColor:{
+        header: '#212529'
+      }
+    },
+  },
+  plugins: [],
+}
+
